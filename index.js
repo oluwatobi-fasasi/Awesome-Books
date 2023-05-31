38e34771d08db4622227887c9e4d3f8ca08721ab
Samuel wanza <s.wanza@alustudent.com>
2023-05-31 10:31:25 +0200

    refactored-index.js and styled the elements in index.css

--- index.js
+++ index.js
@@ -1,57 +1,61 @@
-class Book{
-    constructor(){
-        this.section = document.getElementById('book-section');
-        this.form = document.querySelector('form');
-        this.titleInput = document.getElementById('title-input');
-        this.authorInput = document.getElementById('author-input');
-        this.DATA = JSON.parse(localStorage.getItem('data')) || [];
+class Book {
+  constructor() {
+    this.section = document.getElementById("book-section");
+    this.form = document.querySelector("form");
+    this.titleInput = document.getElementById("title-input");
+    this.authorInput = document.getElementById("author-input");
+    this.DATA = JSON.parse(localStorage.getItem("data")) || [];
+    this.counter = 0;
+    if (this.DATA.length > 0) {
+      this.show();
+    }
+  }
+
+  add(author, title) {
+    this.DATA.push({ author, title });
+    localStorage.setItem("data", JSON.stringify(this.DATA));
+  }
+  remove(author, title) {
+    this.DATA = this.DATA.filter(
+      (b) => b.author !== author || b.title !== title
+    );
+    localStorage.setItem("data", JSON.stringify(this.DATA));
+    this.counter = this.counter - 1;
+  }
+
+  show() {
+    this.section.innerHTML = "";
+    this.DATA.forEach((data) => {
+      this.counter = this.counter + 1;
+      const div = document.createElement("div");
+      div.classList.add("book-item");
+      const authorName = document.createElement("p");
+      authorName.textContent = ` "${data.title}" by ${data.author}`;
+      const removeBtn = document.createElement("button");
+      removeBtn.textContent = "Remove";
+      div.appendChild(authorName);
+      div.appendChild(removeBtn);
+      if (this.counter % 2 === 0) {
+        div.style.background = "lightgray";
+      }
+
+      this.section.appendChild(div);
+
+      removeBtn.addEventListener("click", () => {
+        this.remove(data.author, data.title);
         this.show();
-    }
+      });
+    });
+  }
 
-    add(author, title){
-        this.DATA.push({ author, title });
-        localStorage.setItem('data', JSON.stringify(this.DATA));
-    }
-    remove(author, title){
-        this.DATA = this.DATA.filter((b) => b.author !== author || b.title !== title);
-        localStorage.setItem('data', JSON.stringify(this.DATA));
-    }
-
-    show(){
-        this.section.innerHTML = '';
-        this.DATA.forEach((data)=>{
-            const div = document.createElement('div');
-            const authorName = document.createElement('p');
-            authorName.textContent = `${data.author}`;
-            const titleName = document.createElement('p');
-            titleName.textContent = `${data.title}`;
-            const removeBtn = document.createElement('button');
-            removeBtn.textContent = 'Remove';
-            const hr = document.createElement('hr');
-
-            div.appendChild(authorName);
-            div.appendChild(titleName);
-            div.appendChild(removeBtn);
-            div.appendChild(hr);
-
-            this.section.appendChild(div);
-
-            removeBtn.addEventListener('click', ()=>{
-                this.remove(data.author, data.title);
-                this.show();
-            })
-        })
-    }
-
-    init(){
-        this.form.addEventListener('submit', (e)=>{
-            e.preventDefault();
-            this.add(this.authorInput.value, this.titleInput.value);
-            this.form.reset();
-            this.show();
-            
-        })
-    }
+  init() {
+    this.form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.add(this.authorInput.value, this.titleInput.value);
+      this.form.reset();
+      this.show();
+    });
+  }
 }
 
 const book = new Book();
